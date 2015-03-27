8fb9e1bc5d78d388b09086d644b9a6894c0aa5d9
Whithistle <voinot.thibaut@gmail.com>
2015-03-27 13:16:52 -0400

    -Resolution Conflit TEmplacement-

--- app/src/main/java/fabrique/gestion/BDD/TableEmplacement.java
+++ app/src/main/java/fabrique/gestion/BDD/TableEmplacement.java
@@ -6,24 +6,11 @@ import java.util.ArrayList;
 
 import fabrique.gestion.Objets.Emplacement;
 
-<<<<<<< HEAD
 public class TableEmplacement extends Ctrl{
-=======
-    private ArrayList<String> emplacements;
->>>>>>> dacac8d9716465a206f1cd8995bf93f3ff077b94
 
     public ArrayList<Emplacement> result;
-
-<<<<<<< HEAD
     private static TableEmplacement instance;
-=======
-    private TableEmplacement() {
-        emplacements = new ArrayList<>();
-        emplacements.add("RC");
-        emplacements.add("SS");
-        emplacements.add("Ch.Froide");
-    }
->>>>>>> dacac8d9716465a206f1cd8995bf93f3ff077b94
+
 
     public static TableEmplacement instance(Context ctxt){
         if(instance == null){
