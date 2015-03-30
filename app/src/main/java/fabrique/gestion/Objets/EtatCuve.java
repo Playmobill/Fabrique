b267eb9bb7db52ecc11d5d0edd2596f10df424f2
Whithistle <voinot.thibaut@gmail.com>
2015-03-30 14:33:41 -0400

    Ajout EtatCuve + TEtatCuve

--- app/src/main/java/fabrique/gestion/Objets/EtatCuve.java
+++ app/src/main/java/fabrique/gestion/Objets/EtatCuve.java
@@ -4,4 +4,18 @@ package fabrique.gestion.Objets;
  * Created by thibaut on 30/03/15.
  */
 public class EtatCuve {
+
+    private int id;
+    private String texte;
+
+    public String getTexte() { return texte; }
+    public int getId() { return id; }
+
+    public void setId(int id) { this.id = id; }
+    public void setTexte(String texte) { this.texte = texte; }
+
+    public EtatCuve(int id, String texte){
+        this.id = id ;
+        this.texte = texte;
+    }
 }
