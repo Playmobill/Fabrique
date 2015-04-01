165bff92e9079d2ed62e8a1e60d5bfdf3df657dc
Whithistle <voinot.thibaut@gmail.com>
2015-04-01 09:10:58 -0400

    Activity Brassin

--- app/src/main/java/fabrique/gestion/ActivityAjouterBrassin.java
+++ app/src/main/java/fabrique/gestion/ActivityAjouterBrassin.java
@@ -1,12 +1,16 @@
 package fabrique.gestion;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 
 public class ActivityAjouterBrassin extends Activity implements View.OnClickListener {
+
+    private Button btnAjouter;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,11 +19,16 @@ public class ActivityAjouterBrassin extends Activity implements View.OnClickList
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
         setContentView(R.layout.activity_ajouter_brassin);
+
+        btnAjouter = (Button)findViewById(R.id.btnAjouter);
+        btnAjouter.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
+        if(v.equals(btnAjouter)){
 
+        }
     }
 
     @Override
