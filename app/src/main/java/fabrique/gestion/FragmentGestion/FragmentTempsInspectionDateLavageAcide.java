2040252dcd4b0e813a9e92a8c19bfc4b5c7721c0
Whithistle <voinot.thibaut@gmail.com>
2015-04-30 13:49:08 -0400

    Resolution bug icone (crash)

--- app/src/main/java/fabrique/gestion/FragmentGestion/FragmentTempsInspectionDateLavageAcide.java
+++ app/src/main/java/fabrique/gestion/FragmentGestion/FragmentTempsInspectionDateLavageAcide.java
@@ -14,6 +14,7 @@ import android.widget.LinearLayout;
 import fabrique.gestion.ActivityAccueil;
 import fabrique.gestion.BDD.TableGestion;
 import fabrique.gestion.FragmentAmeliore;
+import fabrique.gestion.R;
 
 public class FragmentTempsInspectionDateLavageAcide extends FragmentAmeliore implements View.OnClickListener{
 
